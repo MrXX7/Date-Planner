5588e8775f77ed5a76c22f1257e519168c4b37e3
Oncu <oohancan@gmail.com>
2023-01-31 16:09:53 +0300

    Initial Commit

---
+++ Date Planner/Date_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_PlannerApp.swift
+//  Date Planner
+//
+//  Created by Oncu Can on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Date_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
